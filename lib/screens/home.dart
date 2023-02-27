49cd8f088ff9621fe38384fbedfb65a75ff6f2de
RiiMJ <99528014+RiiMJ@users.noreply.github.com>
2023-02-27 22:58:25 +0530

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,6 +6,7 @@ import 'package:project2/color.dart';
 import 'package:project2/data.dart';
 import 'package:project2/widgets/category_item.dart';
 import 'package:project2/widgets/pet_item.dart';
+import 'package:project2/screens/detailsScreen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -110,24 +111,6 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  getPets() {
-    double width = MediaQuery.of(context).size.width * .8;
-    return CarouselSlider(
-        options: CarouselOptions(
-          height: 400,
-          enlargeCenterPage: true,
-          disableCenter: true,
-          viewportFraction: .8,
-        ),
-        items: List.generate(
-            pets.length,
-            (index) => PetItem(
-                  data: pets[index],
-                  width: width,
-                  onTap: () {},
-                )));
-  }
-
   int selectedCategory = 0;
   getCategories() {
     List<Widget> lists = List.generate(
@@ -146,5 +129,68 @@ class _HomePageState extends State<HomePage> {
       padding: const EdgeInsets.only(bottom: 5, left: 15),
       child: Row(children: lists),
     );
+  }
+
+  getPets() {
+    List<Map> dogs = [];
+    dogs.addAll(pets);
+    dogs.retainWhere((item) => item['category'] == 'dog');
+
+    List<Map> cats = [];
+    cats.addAll(pets);
+    cats.retainWhere((item) => item['category'] == 'cat');
+
+    List<Map> parrots = [];
+    parrots.addAll(pets);
+    parrots.retainWhere((item) => item['category'] == 'parrot');
+
+    List<Map> rabbits = [];
+    rabbits.addAll(pets);
+    rabbits.retainWhere((item) => item['category'] == 'rabbit');
+
+    List<Map> fish = [];
+    fish.addAll(pets);
+    fish.retainWhere((item) => item['category'] == 'fish');
+
+    List<Map> turtle = [];
+    turtle.addAll(pets);
+    turtle.retainWhere((item) => item['category'] == 'turtle');
+
+    double width = MediaQuery.of(context).size.width * .8;
+    List<Map> selectList = [];
+
+    if (selectedCategory == 0) {
+      selectList = pets;
+    } else if (selectedCategory == 1) {
+      selectList = dogs;
+    } else if (selectedCategory == 2) {
+      selectList = cats;
+    } else if (selectedCategory == 3) {
+      selectList = parrots;
+    } else if (selectedCategory == 4) {
+      selectList = rabbits;
+    } else if (selectedCategory == 5) {
+      selectList = fish;
+    } else if (selectedCategory == 6) {
+      selectList = turtle;
+    }
+
+    for (int i = 0; i < categories.length; i++) {}
+    return CarouselSlider(
+        options: CarouselOptions(
+          height: 400,
+          enlargeCenterPage: true,
+          disableCenter: true,
+          viewportFraction: .8,
+        ),
+        items: List.generate(
+            selectList.length,
+            (index) => PetItem(
+                  data: selectList[index],
+                  width: width,
+                  onTap: () {
+                    detailsScreen(id: selectList[index]['id']);
+                  },
+                )));
   }
 }
